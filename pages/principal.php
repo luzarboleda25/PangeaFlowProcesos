edadd63ae39af490d4fd9bdab80300bee19b97ea
Mariela Arboleda <luz.arboleda25@gmail.com>
2014-01-10 17:26:22 -0430

    arreglando

--- pages/principal.php
+++ pages/principal.php
@@ -7,12 +7,13 @@
   $client = new SOAPClient($wsdl_url);
   $client->decode_utf8 = false; 
 if (isset($_POST["Biniciar"])) {
+echo number_format(12.456, 2, '.', '');
 	 	if(isset($_POST["usuario"]) && $_POST["usuario"]!="" && isset($_POST["password"]) && $_POST["password"]!="" ){	
 				$usuario= array('id' => $_POST["usuario"],'clave' => $_POST["password"],'borrado'=>'0','diasValidezClave'=>'10');		
 			    $sesion= array('idUsuario' =>$usuario,'ip'=>'127.0.0.1','borrado'=>'0');
 				$sesionUsuario=array('sesionActual' =>$sesion);
 				$resultadoLogueo = $client->LogIn($sesionUsuario);
-				//echo '<pre>';print_r($resultadoLogueo);	
+				echo '<pre>';print_r($resultadoLogueo);	
 			   if($resultadoLogueo->return->estatus=="OK"){ 
 				$periodo= array('id' => '2','borrado'=>'0');		
 				$grupo= array('id' => '2','borrado'=>'0');		
@@ -30,9 +31,9 @@ if (isset($_POST["Biniciar"])) {
 				$client = new SOAPClient($wsdl_url);
 				$client->decode_utf8 = false; 	
 				$resultadoCreacion=$client->CrearInstancia($parametros);
-			   // echo '<br><pre>';print_r($resultadoCreacion);
+			    echo '<br><pre>';print_r($resultadoCreacion);
 					if($resultadoCreacion->return->estatus=="OK"){
-						$resultadoInstancia=$client->consultarInstanciaXMaxId();
+						$resultadoInstancia=$client->consultarInstanciaXMaxId($usuario);
 						if($resultadoInstancia->return->estatus=="OK"){
 							$wsdl_url = 'http://localhost:15362/CapaDeServiciosAdmin/GestionDeActividades?WSDL';
 							$client = new SOAPClient($wsdl_url);
@@ -40,34 +41,25 @@ if (isset($_POST["Biniciar"])) {
 							$instancia= array('id' => $resultadoInstancia->return->instancias->id,'borrado'=>'0');	
 							$parametros= array('idInstancia' => $instancia,'idTarea'=>$tareaInicial);	
 							$resultadoActividad=$client->consultarActividadXInstanciaYTarea($parametros);
-						//	echo '<br><pre>';print_r($resultadoActividad);
 							if($resultadoActividad->return->estatus=="OK"){
 								$actividad= array('id' => $resultadoActividad->return->actividads->id,'borrado'=>'0');	
 							    $parametros= array('actividadActual' => $actividad,
-							           'usuarioActual' => $usuario);
-								$resultadoAsignar=$client->AsignarActividad($parametros);
-echo '<br><pre>';print_r($resultadoAsignar);								
-								$parametros= array('actividadActual' => $actividad,
-							           'sesionActual' => $sesionActual);								
+							           'sesionActual' => $sesionActual);
 								$resultadoInicio=$client->IniciarActividad($parametros);
-								echo '<br><pre>';print_r($resultadoInicio);
-								if($resultadoInicio->return->estatus=="OK"){
+								if($resultadoInicio->return->estatus=="Ok"){
 									$condicion= array('id' => '1','borrado'=>'0');	
 									 $parametros= array('actividadActual' => $actividad,
-							           'sesionActual' => $sesionActual,
-									   'condicionActual'=>$condicion);
+							           'actividadActual' => $sesionActual);
 									$resultadoFinActividad=$client->FinalizarActividad($parametros);
-									echo '<br><pre>';print_r($resultadoFinActividad);
 									if($resultadoFinActividad->return->estatus=="OK"){
 									/*	$wsdl_url = 'http://localhost:15362/CapaDeServiciosAdmin/GestionDeTransicion?WSDL';
 										$client = new SOAPClient($wsdl_url);
 										$client->decode_utf8 = false; 	
 										$resultadoTransicion=$client->ConsultaTransicion($tareaInicial);
-										if($resultadoTransicion->return->estatus="OK"){
-										  $siguienteTarea=$resultadoTransicion->return->transicions->idTareaDestino;
-										 
-										}*/
-										 iraURL("../pages/principal.php");
+										if($resultadoTransicion->return->estatus="OK"){*/
+										//  $siguienteTarea=$resultadoTransicion->return->transicions->idTareaDestino;
+										  
+										}
 									}
 								}	
 							}
@@ -79,7 +71,7 @@ echo '<br><pre>';print_r($resultadoAsignar);
 				
 	    }
 	}	
-  include("../views/index.php");
+  include("../views/principal.php");
 /*  } catch (Exception $e) {
 	javaalert('Lo sentimos no hay conexión');
 	//iraURL('../views/index.php');	
---
+++ views/principal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>PangeaFlow</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!-- javascript -->
+	<script type="text/javascript" src="../js/jquery-2.0.3.js"></script>
+	<script type='text/javascript' src="../js/bootstrap.js"></script>
+	<!-- styles -->
+	<link href="../css/bootstrap.css" rel="stylesheet">
+	<link href="../css/bootstrap-theme.css" rel="stylesheet"> 
+</head>
+
+<body>	
+	  <div class="container app-container">
+     		<form class="form-signin" method="post">
+        		<h3 class="form-signin-heading">Presione para procesar la información del cliente</h3>
+        		<button class="btn btn-large btn-info" type="submit" name="Procesar información del cliente">Iniciar Sesión</button>
+      		</form>
+    	</div>
+    </body>
+</html>
